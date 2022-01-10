f69ee2edb79211789fc6141eddd5ee2c4d2b95cb
Kotchakorn M <32461094+mill1704@users.noreply.github.com>
2022-01-10 23:15:44 +0700

    gulp
    run --> gulp watchSass

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const { src, dest, watch } = require('gulp');
+const gulp = require('gulp-sass');
+const prefix = require("gulp-autoprefixer");
+
+const scss = './assets/scss/**/*.scss'
+const css = './assets/css'
+
+// for compile bootstrap plugins
+// const scss = './assets/plugins/bootstrap/scss/*.scss'
+// const css = './assets/plugins/bootstrap/css'
+
+function compileSass(done) {
+  src(scss)
+    .pipe(gulp().on('error', gulp.logError))
+    .pipe(prefix())
+    .pipe(dest(css))
+  done()
+}
+
+function watchSass() {
+  watch(scss, compileSass);
+}
+
+exports.watchSass = watchSass
+
+// npm install gulp --save-deps && npm install gulp-sass --save-deps
+// gulp watchSass
